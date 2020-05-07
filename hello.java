2f286550bc9f63590ded3050b840fce28080c3cf
drashti4 <dpandya@isimtl.local>
2020-05-07 19:28:48 -0400

    added gitignore

--- hello.java
+++ hello.java
@@ -1,6 +1,5 @@
-public class Hello {
-   public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World");
-   }
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("This will be printed");
+    }
 }
